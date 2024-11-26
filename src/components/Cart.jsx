57b7a3a0e079562c811c7d3c0a58a7a3e204400c
emanuelginer <emanuelginer77@gmail.com>
2024-11-26 12:51:34 -0300

    Change structure of the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,6 @@
-import { useId, useContext, useMemo } from 'react';
-import { ShoppingCart } from 'lucide-react';
+import { useId, useMemo } from 'react';
+import { ShoppingCart, Trash2 } from 'lucide-react';
 import "../styles/Cart.css";
-import { CartContext } from '../contexts/CartContext';
 import { useCart } from '../hooks/useCart';
 
 
@@ -20,30 +19,37 @@ const Cart = () => {
       </label>
       <input id={cartCheckboxId} type="checkbox" hidden/>
       <aside className='cart'>
-        <ul>
-          { cart?.map(product => (
-            <li key={product.id}>
+        
+        { cart?.map(product => (
+          <div className='cart-item-container' key={product.id}>
             <img src={product.image} alt={product.title}/>
-            <div>
-              <strong>{product.title}</strong> - $ {product.price}
+            <div  className='cart-item'>
+              <div className='cart-item-info'>
+                <div className='cart-item-title'>{product.title}</div> 
+              </div>
+              <div className='cart-item-details'>
+                <span className='cart-item-quantity'>Qty: {product.quantity}</span>
+                <span className='cart-item-price'>$ {(product.totalPrice).toFixed(2)}</span>
+              </div>
+              <button className='cart-item-remove-btn'
+                onClick={() => removeFromCart(product)}>
+                  
+                  <Trash2 />
+              </button>
             </div>
-            <footer>
-              <small>
-                Qty: {product.quantity}
-              </small>
-              <small>
-                SubTotal: $ {(product.totalPrice).toFixed(2)}
-              </small>
-              <button onClick={() => removeFromCart(product)}>Remove</button>
-            </footer>
-          </li>  
-          ))|| <p>No items in the cart</p>}
+          </div>
           
-        </ul>
-        <small>Total: $ {total}</small>
-        <button onClick={clearCart}>
-          <ShoppingCart />
-        </button>
+        ))|| <p>No items in the cart</p>}
+          <p className='cart-total'>
+            Total: ${total.toFixed(2)}
+          </p>   
+        <div className='cart-item-actions'>
+          <button onClick={clearCart} className='clear-cart-btn'>
+            Clear cart <Trash2 />
+          </button>
+          <button className='checkout-button'>Checkout</button>
+        </div>
+        
       </aside>
     </>
   )
